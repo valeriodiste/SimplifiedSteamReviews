d89bd87f7b5442b218f1b70ec7b76326bc69260d
valeriodiste <103375912+valeriodiste@users.noreply.github.com>
2024-10-09 21:15:36 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ let reviews = [];
 
 // Function that is called when the DOM is loaded
 document.addEventListener("DOMContentLoaded", function () {
+	// If the page is opened with an "appid" URL parameter, automatically fill the "game" input with the appid
+	let urlParams = new URLSearchParams(window.location.search);
+	let appid = urlParams.get("appid");
+	if (appid !== null) {
+		// Fill the "game" input with the appid
+		document.getElementsByName("game")[0].value = appid;
+		// Store the last appid
+		lastAppid = appid;
+		// Send the request to the server
+		get_reviews(appid, true, "all");
+	}
 	// On click of the "submit" button on the form, send the form data to the server
 	document.getElementById("submit").addEventListener("click", function (e) {
 		// Prevent the default form submission
@@ -15,37 +26,18 @@ document.addEventListener("DOMContentLoaded", function () {
 			alert("Please provide a game!");
 			return;
 		}
-		// API url
-		let apiURL = "https://panecaldoaldo.pythonanywhere.com/steam_reviews"
 		// Send the form data as URL parameters to the server (take the "game", "english-only" and "review-type" values from the form)
 		let game = document.getElementsByName("game")[0].value;
 		let englishOnly = document.getElementsByName("english-only")[0].checked;
 		let reviewType = document.getElementsByName("review-type")[0].value;
-		// Construct the URL parameters ("appid", "english_only", "review_type")
-		let urlParams = new URLSearchParams();
 		// If the "game" is a numeric value, set the "appid" to the value of the "game", otherwise its a link, so extract the "appid" from the link
 		let appid;
 		if (!isNaN(game)) appid = game;
 		else appid = game.split("app/")[1].split("/")[0];
 		// Store the last appid
 		lastAppid = appid;
-		// Append the URL parameters
-		urlParams.append("appid", appid);
-		urlParams.append("english_only", englishOnly);
-		urlParams.append("review_type", reviewType);
-		// Fetch the API URL with the URL parameters
-		let requestURL = apiURL + "?" + urlParams.toString();
-		console.log("Sending request: " + requestURL);
-		fetch(requestURL)
-			.then(response => response.json())
-			.then(data => {
-				// Display the data in the "response" div
-				console.log(data);
-				// Store the reviews
-				reviews = data;
-				// Display the reviews
-				displayReviews();
-			});
+		// Send the request to the server
+		get_reviews(appid, englishOnly, reviewType);
 	});
 	// On click of the "sort-by" select, display the reviews
 	document.getElementById("sort-by").addEventListener("change", function () {
@@ -64,8 +56,61 @@ document.addEventListener("DOMContentLoaded", function () {
 	// });
 });
 
+function get_reviews(appid, english_only, review_type) {
+	// API url
+	let apiURL = "https://panecaldoaldo.pythonanywhere.com/steam_reviews";
+	// Construct the URL parameters ("appid", "english_only", "review_type")
+	let urlParams = new URLSearchParams();
+	// Append the URL parameters
+	urlParams.append("appid", appid.toString());
+	urlParams.append("english_only", english_only.toString());
+	urlParams.append("review_type", review_type.toString());
+	// Fetch the API URL with the URL parameters
+	let requestURL = apiURL + "?" + urlParams.toString();
+	console.log("Sending request: " + requestURL);
+	fetch(requestURL)
+		.then(response => response.json())
+		.then(data => {
+			// Display the data in the "response" div
+			console.log(data);
+			// Store the reviews
+			reviews = data;
+			// Display the reviews
+			displayReviews();
+		});
+}
+
 // Function to display the reviews
 function displayReviews() {
+	/*
+	{
+		"recommendationid": "176074338",
+		"author": {
+			"steamid": "76561199199077897",
+			"num_games_owned": 0,
+			"num_reviews": 3,
+			"playtime_forever": 403,
+			"playtime_last_two_weeks": 158,
+			"playtime_at_review": 301,
+			"last_played": 1727744100
+		},
+		"language": "english",
+		"review": "funny dice, many many dots. Scaling goes insane. Much big numerals. Going for 100e",
+		"timestamp_created": 1727677994,
+		"timestamp_updated": 1727677994,
+		"voted_up": true,
+		"votes_up": 0,
+		"votes_funny": 0,
+		"weighted_vote_score": 0,
+		"comment_count": 0,
+		"steam_purchase": true,
+		"received_for_free": false,
+		"written_during_early_access": false,
+		"hidden_in_steam_china": true,
+		"steam_china_location": "",
+		"primarily_steam_deck": false
+	}
+	*/
 	// Auxiliary function to get a review given the username, data, recommended bool value and text
 	function get_review_element(username, date, recommended, text) {
 		/*
@@ -145,33 +190,3 @@ function displayReviews() {
 	}
 
 }
-
-/*
-{
-	"recommendationid": "176074338",
-	"author": {
-		"steamid": "76561199199077897",
-		"num_games_owned": 0,
-		"num_reviews": 3,
-		"playtime_forever": 403,
-		"playtime_last_two_weeks": 158,
-		"playtime_at_review": 301,
-		"last_played": 1727744100
-	},
-	"language": "english",
-	"review": "funny dice, many many dots. Scaling goes insane. Much big numerals. Going for 100e",
-	"timestamp_created": 1727677994,
-	"timestamp_updated": 1727677994,
-	"voted_up": true,
-	"votes_up": 0,
-	"votes_funny": 0,
-	"weighted_vote_score": 0,
-	"comment_count": 0,
-	"steam_purchase": true,
-	"received_for_free": false,
-	"written_during_early_access": false,
-	"hidden_in_steam_china": true,
-	"steam_china_location": "",
-	"primarily_steam_deck": false
-}
-*/
