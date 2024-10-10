497bd6725cbba8d70075034b4cd29ab16c5ca686
valeriodiste <103375912+valeriodiste@users.noreply.github.com>
2024-10-10 14:46:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -134,11 +134,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function getSimpleReviewsPageHTML() {
 	let addLoremIpsum = true;
+	let loremIpsumTitle = "Reviews";
 	let loremIpsumText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 	let newTabHTML =
-		"<html style='margin: 0; color: white;'><head><title>Reviews</title><style>*{ box-sizing: border-box;}</style></head><body style=\"background-color: black; font-family:'Open Sans', sans-serif; font-size: 14px; margin: 0; padding: 1.5em 1.5em;\">" +
-		(addLoremIpsum ? "<p>" + loremIpsumText + "</p>" : "") +
-		"<div id='reviews-container' style='width: 100%;'>" + getReviewsHTML(true) + "</div></body></html>";
+		"<html style='margin: 0; color: white;'><head><title>Reviews</title><style>*{ box-sizing: border-box;}</style></head><body style=\"background-color: black; font-family:'Open Sans', sans-serif; font-size: 14px; margin: 0; padding: 1.5em 1.5em;\"><article>" +
+		(addLoremIpsum ? "<h2>" + loremIpsumTitle + "</h2><p>" + loremIpsumText + "</p>" : "") +
+		"<div id='reviews-container' style='width: 100%;'>" + getReviewsHTML(true) + "</div>" +
+		"</article></body></html>";
 	return newTabHTML;
 }
 
